6a1d529a23d3c19e6c7952c3f736b48cdc844575
yblanken <yuri.blankenstein@asml.com>
2026-07-20 10:18:32 +0200

    Improved the TMSC reconstructor example

--- plugins/nl.esi.pps.tmsc.reconstruct.ui/src/nl/esi/pps/tmsc/reconstruct/ui/handlers/ReconstructHandler.java
+++ plugins/nl.esi.pps.tmsc.reconstruct.ui/src/nl/esi/pps/tmsc/reconstruct/ui/handlers/ReconstructHandler.java
@@ -9,31 +9,41 @@
  */
 package nl.esi.pps.tmsc.reconstruct.ui.handlers;
 
+import static org.eclipse.core.runtime.IStatus.ERROR;
+
 import java.io.IOException;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Named;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Evaluate;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.lsat.common.emf.common.util.URIHelper;
+import org.eclipse.lsat.common.emf.ecore.resource.Persistor;
+import org.eclipse.lsat.common.emf.ecore.resource.PersistorFactory;
 import org.eclipse.swt.widgets.Shell;
 
+import nl.esi.pps.common.core.runtime.ErrorStatusException;
+import nl.esi.pps.common.core.runtime.FailOnErrorStatus;
+import nl.esi.pps.common.core.runtime.jobs.JobUtils;
+import nl.esi.pps.common.ide.ui.jobs.StatusReportingJob;
+import nl.esi.pps.tmsc.TmscPlugin;
 import nl.esi.pps.tmsc.reconstruct.TmscTraceParser;
 import nl.esi.pps.tmsc.reconstruct.TmscTraceReconstructor;
 
 public class ReconstructHandler {
+	private static final String PLUGIN_ID = "nl.esi.pps.tmsc.reconstruct.ui";
+
 	@Evaluate
 	@CanExecute
 	public boolean canExecute(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection selection) {
@@ -41,8 +51,8 @@ public class ReconstructHandler {
 			return false;
 		}
 		Object selectedElement = selection.getFirstElement();
-		if (selectedElement instanceof IFile) {
-			String fileExtension = ((IFile) selectedElement).getFileExtension();
+		if (selectedElement instanceof IFile selectedFile) {
+			String fileExtension = selectedFile.getFileExtension();
 			return "tmsctrace".equals(fileExtension);
 		}
 		return false;
@@ -51,28 +61,43 @@ public class ReconstructHandler {
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_SELECTION) IStructuredSelection selection,
 			@Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
-		try {
-			reconstruct((IFile) selection.getFirstElement());
-		} catch (Exception e) {
-			MessageDialog.openError(shell, "Reconstruction failed", e.getLocalizedMessage());
-		}
+		IFile tmsctraceFile = (IFile) selection.getFirstElement();
+		StatusReportingJob.runUserJob("Reconstruct TMSC", monitor -> reconstruct(tmsctraceFile, monitor), PLUGIN_ID);
 	}
 
-	private void reconstruct(IFile traceIFile) throws IOException, CoreException {
-		// Reconstruction
+	private static IStatus reconstruct(IFile traceIFile, IProgressMonitor monitor) throws ErrorStatusException {
+		SubMonitor subMonitor = SubMonitor.convert(monitor, 101);
+		subMonitor.setTaskName("Reconstructing TMSC...");
+
+		FailOnErrorStatus result = new FailOnErrorStatus(PLUGIN_ID, "Reconstructed TMSC");
+		result.setWarningMessage("Reconstructed TMSC with warnings.");
+		result.setErrorMessage("Failed to reconstructed TMSC");
+
 		TmscTraceReconstructor reconstructor = new TmscTraceReconstructor();
-		TmscTraceParser.parse(traceIFile.getContents(), reconstructor);
+		try {
+			TmscTraceParser.parse(traceIFile.getContents(), reconstructor);
+			subMonitor.worked(50);
+		} catch (Exception e) {
+			result.add(new Status(ERROR, PLUGIN_ID,
+					String.format("Failed to parse %s: %s", traceIFile.getFullPath(), e.getMessage()), e));
+		}
 
 		// Saving models to file
-		URI traceURI = URI.createPlatformResourceURI(traceIFile.getFullPath().toString(), true);
-		URI tmscURI = traceURI.trimFileExtension().appendFileExtension("tmsc");
-		ResourceSet resourceSet = new ResourceSetImpl();
-		Resource tmscResource = resourceSet.createResource(tmscURI);
-		tmscResource.getContents().add(reconstructor.getTmsc());
-		tmscResource.getContents().add(reconstructor.getArchitecture());
-		tmscResource.save(Collections.emptyMap());
-		
+		IFile tmscIFile = JobUtils.getSibling(traceIFile, null, TmscPlugin.TMSC_FILE_EXTENSION_BINARY_COMPRESSED);
+		Persistor<?> persistor = new PersistorFactory().getPersistor();
+		try {
+			Map<Object, Object> saveOptions = new HashMap<>();
+			saveOptions.put(IProgressMonitor.class, subMonitor.split(50));
+			persistor.save(URIHelper.asURI(tmscIFile), saveOptions, reconstructor.getTmsc(),
+					reconstructor.getArchitecture());
+		} catch (IOException e) {
+			result.add(new Status(ERROR, PLUGIN_ID,
+					String.format("Failed to save %s: %s", tmscIFile.getFullPath(), e.getMessage()), e));
+		}
+
 		// Refresh workspace to view created file
-		traceIFile.getParent().refreshLocal(IResource.DEPTH_ONE, null);
+		JobUtils.refreshWorkspaceProjects(subMonitor.split(1), traceIFile);
+
+		return result;
 	}
 }
